c4031e888fbe674facac39d1dc170aac57b41efc
Byron Martinez <mr.byronmartinez@gmail.com>
2019-07-16 16:42:56 -0700

    tutorial 5 completed on dunder methods

--- magic_dunder_methods.py
+++ magic_dunder_methods.py
@@ -15,6 +15,31 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first,
+                                                   self.last,
+                                                   self.pay
+                                                   )
 
-dev_1 = Employee('Byron', 'Martienz', 50000, 'Python')
-dev_2 = Employee('Yolanda', 'Martienz', 60000, 'java')
+    def __str__(self):
+        return '{} - {}'.format(self.first,
+                                self.last
+                                )
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
+
+
+dev_1 = Employee('Byron', 'Martienz', 50000)
+dev_2 = Employee('Yolanda', 'Martienz', 60000)
+
+# print(dev_1)
+# print(repr(dev_1))
+# # print(str(dev_1))
+#
+# print(dev_1 + dev_2)
+
+print(len(dev_1))
